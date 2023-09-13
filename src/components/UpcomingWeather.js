34703ab09068fbdba1a88e7b7598aa8e85d4b56b
Taha <tahaaykiroglu99@gmail.com>
2023-09-13 21:52:36 +0300

    Update UpcomingWeather.js

--- src/components/UpcomingWeather.js
+++ src/components/UpcomingWeather.js
@@ -1,5 +1,13 @@
 import React from "react";
-import { SafeAreaView, StyleSheet, Text, FlatList, View } from "react-native";
+import {
+  SafeAreaView,
+  StyleSheet,
+  Text,
+  FlatList,
+  View,
+  StatusBar,
+  ImageBackground,
+} from "react-native";
 import { Feather } from "@expo/vector-icons";
 
 const DATA = [
@@ -44,11 +52,11 @@ const DATA = [
 const Item = (props) => {
   const { dt_txt, min, max, condition } = props;
   return (
-    <View>
+    <View style={styles.item}>
       <Feather name={"sun"} size={50} color={"white"} />
-      <Text>{dt_txt}</Text>
-      <Text>{min}</Text>
-      <Text>{max}</Text>
+      <Text style={styles.date}>{dt_txt}</Text>
+      <Text style={styles.temp}>{min}</Text>
+      <Text style={styles.temp}>{max}</Text>
     </View>
   );
 };
@@ -63,19 +71,47 @@ const UpcomingWeather = () => {
     />
   );
   return (
-    <SafeAreaView styles={styles.container}>
-      <Text>Upcoming Weather</Text>
-      <FlatList
-        data={DATA}
-        renderItem={renderItem}
-        keyExtractor={(item) => item.dt_txt}
-      />
+    <SafeAreaView style={styles.container}>
+      <ImageBackground
+        source={require("../../assets/upcoming-background.jpg")}
+        style={styles.image}
+      >
+        <Text>Upcoming Weather</Text>
+        <FlatList
+          data={DATA}
+          renderItem={renderItem}
+          keyExtractor={(item) => item.dt_txt}
+        />
+      </ImageBackground>
     </SafeAreaView>
   );
 };
 
 const styles = StyleSheet.create({
   container: {
+    flex: 1,
+    marginTop: StatusBar.currentHeight || 0,
+    backgroundColor: "royalblue",
+  },
+  item: {
+    padding: 20,
+    marginVertical: 8,
+    marginHorizontal: 16,
+    flexDirection: "row",
+    justifyContent: "space-around",
+    alignItems: "center",
+    borderWidth: 5,
+    backgroundColor: "pink",
+  },
+  temp: {
+    color: "white",
+    fontSize: 20,
+  },
+  date: {
+    color: "white",
+    fontSize: 15,
+  },
+  image: {
     flex: 1,
   },
 });
