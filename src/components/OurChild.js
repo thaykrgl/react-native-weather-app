27d37c442d2de8effef3111950e94ac170c40671
Taha <tahaaykiroglu99@gmail.com>
2023-09-13 21:52:30 +0300

    Create OurChild.js

---
+++ src/components/OurChild.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { View, Text } from "react-native";
+
+const OurChild = () => {
+  return (
+    <View style={{ height: 200, width: 200, backgroundColor: "red" }}>
+      <Text></Text>
+    </View>
+  );
+};
