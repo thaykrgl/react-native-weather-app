739089b85a43b89eb114a88e7d8be2e16251d42f
Taha <tahaaykiroglu99@gmail.com>
2023-09-14 13:33:04 +0300

    Create IconText.js

---
+++ src/components/IconText.js
@@ -0,0 +1,22 @@
+import React from "react";
+import { View, Text, StyleSheet } from "react-native";
+import { Feather } from "@expo/vector-icons";
+
+const IconText = (props) => {
+  const { iconName, iconColor, bodyText, bodyTextStyles } = props;
+
+  return (
+    <View>
+      <Feather name={iconName} size={50} color={iconColor} />
+      <Text style={[styles.textTheme, bodyTextStyles]}>{bodyText}</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  textTheme: {
+    fontWeight: "bold",
+  },
+});
+
+export default IconText;
