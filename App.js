0af5b5c7a6e99bb0b9eb1f77aff487f23fd37d46
Taha <tahaaykiroglu99@gmail.com>
2023-09-14 13:32:59 +0300

    Update App.js

--- App.js
+++ App.js
@@ -2,11 +2,12 @@ import React from "react";
 import { View, StyleSheet } from "react-native";
 import CurrentWeather from "./src/screens/CurrentWeather";
 import UpcomingWeather from "./src/screens/UpcomingWeather";
+import City from "./src/screens/City";
 
 const App = () => {
   return (
     <View style={styles.container}>
-      <UpcomingWeather />
+      <City />
     </View>
   );
 };
