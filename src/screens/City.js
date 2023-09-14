17da96254d41c355d7004cd856a62f9fe59e6a4d
Taha <tahaaykiroglu99@gmail.com>
2023-09-14 13:33:07 +0300

    Update City.js

--- src/screens/City.js
+++ src/screens/City.js
@@ -5,7 +5,10 @@ import {
   StyleSheet,
   StatusBar,
   Text,
+  View,
 } from "react-native";
+import { Feather } from "@expo/vector-icons";
+import IconText from "../components/IconText";
 
 const City = () => {
   return (
@@ -16,6 +19,22 @@ const City = () => {
       >
         <Text style={[styles.cityName, styles.cityText]}>London</Text>
         <Text style={[styles.countryName, styles.cityText]}>UK</Text>
+        <View style={styles.populationWrapper}>
+          <IconText
+            iconName={"sunrise"}
+            iconColor={"white"}
+            bodyText={"10:46:58am"}
+            bodyTextStyles={styles.riseSetText}
+          />
+        </View>
+        <View style={styles.riseSetWrapper}>
+          <IconText
+            iconName={"sunset"}
+            iconColor={"white"}
+            bodyText={"17:28:15pm"}
+            bodyTextStyles={styles.riseSetText}
+          />
+        </View>
       </ImageBackground>
     </SafeAreaView>
   );
@@ -40,6 +59,27 @@ const styles = StyleSheet.create({
     alignSelf: "center",
     color: "white",
     fontWeight: "bold",
+  },
+  populationWrapper: {
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "center",
+    marginTop: 30,
+  },
+  populationText: {
+    fontSize: 25,
+    marginLeft: 7.5,
+    color: "red",
+  },
+  riseSetWrapper: {
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-around",
+    marginTop: 30,
+  },
+  riseSetText: {
+    fontSize: 20,
+    color: "white",
   },
 });
 
