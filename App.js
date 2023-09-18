e2571b58af1c28fd48ebefd4746ac8f82849de5f
Taha <tahaaykiroglu99@gmail.com>
2023-09-18 22:11:56 +0300

    Update App.js

--- App.js
+++ App.js
@@ -1,25 +1,14 @@
 import React from "react";
-import CurrentWeather from "./src/screens/CurrentWeather";
-import UpcomingWeather from "./src/screens/UpcomingWeather";
-import City from "./src/screens/City";
 import { NavigationContainer } from "@react-navigation/native";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import Tabs from "./src/components/Tabs";
 
 const Tab = createBottomTabNavigator();
 
 const App = () => {
   return (
     <NavigationContainer>
-      <Tab.Navigator
-        screenOptions={{
-          tabBarActiveTintColor: "tomato",
-          tabBarInactiveTintColor: "grey",
-        }}
-      >
-        <Tab.Screen name={"Current"} component={CurrentWeather} />
-        <Tab.Screen name={"Upcoming"} component={UpcomingWeather} />
-        <Tab.Screen name={"City"} component={City} />
-      </Tab.Navigator>
+      <Tabs />
     </NavigationContainer>
   );
 };
