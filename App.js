4e9e9decf64597c0fb5d2140b2a4185496a460a5
Taha <tahaaykiroglu99@gmail.com>
2023-09-19 08:04:43 +0300

    Update App.js

--- App.js
+++ App.js
@@ -2,13 +2,14 @@ import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import Tabs from "./src/components/Tabs";
+import Counter from "./src/demonstration/Counter";
 
 const Tab = createBottomTabNavigator();
 
 const App = () => {
   return (
     <NavigationContainer>
-      <Tabs />
+      <Counter />
     </NavigationContainer>
   );
 };
