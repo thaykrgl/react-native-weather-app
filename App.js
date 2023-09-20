20120fa525b710e59277007aac644ef5fdade92f
Taha <tahaaykiroglu99@gmail.com>
2023-09-20 08:06:04 +0300

    Update App.js

--- App.js
+++ App.js
@@ -1,17 +1,61 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { ActivityIndicator, View, StyleSheet } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
-import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import Tabs from "./src/components/Tabs";
-import Counter from "./src/demonstration/Counter";
-
-const Tab = createBottomTabNavigator();
+import * as Location from "expo-location";
+import { WEATHER_API_KEY } from "@env";
 
 const App = () => {
+  const [loading, setLoading] = useState(true);
+  const [location, setLocation] = useState(null);
+  const [error, setError] = useState(null);
+  const [weather, setWeather] = useState([]);
+
+  const fetchWeatherData = async () => {
+    try {
+      const res = await fetch(
+        `http://api.openweathermap.org/data/2.5/forecast?lat=${location.coords.latitude}&lon=${location.coords.longitude}&appid=${WEATHER_API_KEY}`
+      );
+      const data = await res.json();
+      setWeather(data);
+      setLoading(false);
+    } catch (error) {
+      setError("Could not fetch weather");
+    }
+  };
+
+  useEffect(() => {
+    (async () => {
+      let { status } = await Location.requestForegroundPermissionsAsync();
+      if (status !== "granted") {
+        setError("permission to access location was denied");
+        return;
+      }
+      let location = await Location.getCurrentPositionAsync();
+      setLocation(location);
+      await fetchWeatherData();
+    })();
+  }, []);
+
+  if (loading) {
+    return (
+      <View style={styles.container}>
+        <ActivityIndicator size={"large"} color={"blue"} />
+      </View>
+    );
+  }
+
   return (
     <NavigationContainer>
-      <Counter />
+      <Tabs />
     </NavigationContainer>
   );
 };
 
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+  },
+});
 export default App;
