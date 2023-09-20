756fdc4af6f35759f5e3086a9c6e4b5c5b240e52
Taha <tahaaykiroglu99@gmail.com>
2023-09-20 08:06:22 +0300

    Update Counter.js

--- src/demonstration/Counter.js
+++ src/demonstration/Counter.js
@@ -1,22 +1,38 @@
-import React, { useState } from "react";
-import { View, StyleSheet, Text, Button } from "react-native";
-
+import React, { useState, useEffect } from "react";
+import { View, StyleSheet, Text, Button, SafeAreaView } from "react-native";
 const Counter = () => {
   const [count, setCount] = useState(0);
+  const [newCount, setNewCount] = useState(0);
+
+  useEffect(() => {
+    console.log(`Count changed`);
+  }, [count]);
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>{`Count: ${count}`}</Text>
-      <Button
-        color={"red"}
-        title={"Increase the count"}
-        onPress={() => setCount(count + 1)}
-      />
-      <Button
-        color={"green"}
-        title={"Decrease the count"}
-        onPress={() => setCount(count - 1)}
-      />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <View>
+        <Text style={styles.title}>{`Count: ${count}`}</Text>
+        <Button
+          color={"red"}
+          title={"Increase the count"}
+          onPress={() => setCount(count + 1)}
+        />
+        <Button
+          color={"green"}
+          title={"Decrease the count"}
+          onPress={() => setCount(count - 1)}
+        />
+        <Button
+          color={"red"}
+          title={"Increase the count"}
+          onPress={() => setNewCount(count + 1)}
+        />
+        <Button
+          color={"green"}
+          title={"Decrease the count"}
+          onPress={() => setNewCount(count - 1)}
+        />
+      </View>
+    </SafeAreaView>
   );
 };
 
