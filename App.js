328773c76ed42d41c3e6bcacdf0facd2211bd445
Taha <tahaaykiroglu99@gmail.com>
2023-09-22 20:15:14 +0300

    Update App.js

--- App.js
+++ App.js
@@ -1,15 +1,13 @@
-import React, { useState, useEffect } from "react";
 import { ActivityIndicator, View, StyleSheet } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import Tabs from "./src/components/Tabs";
-import * as Location from "expo-location";
-import { WEATHER_API_KEY } from "@env";
 import { useGetWeather } from "./src/hooks/useGetWeather";
+import ErrorItem from "./src/components/ErrorItem";
 
 const App = () => {
   const [loading, error, weather] = useGetWeather();
 
-  if (weather && weather.list) {
+  if (weather && weather.list && !loading) {
     return (
       <NavigationContainer>
         <Tabs weather={weather} />
@@ -19,7 +17,11 @@ const App = () => {
 
   return (
     <View style={styles.container}>
-      <ActivityIndicator size={"large"} color={"blue"} />
+      {error ? (
+        <ErrorItem />
+      ) : (
+        <ActivityIndicator size={"large"} color={"blue"} />
+      )}
     </View>
   );
 };
